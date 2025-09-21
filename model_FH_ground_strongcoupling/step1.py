f9abac4131883ca30654325ddbb1469148d50286
CYI1995 <90730278+CYI1995@users.noreply.github.com>
2025-09-21 20:57:59 +0800

    Add files via upload

---
+++ model_FH_ground_strongcoupling/step1.py
@@ -0,0 +1,186 @@
+import numpy as np
+import math
+import scipy
+from matplotlib import pyplot as plt
+import random
+
+import scipy.linalg
+import source as mycode
+import statistics
+import cmath
+
+# def h(eps,k_temp,t):
+
+#     return eps*math.cos(k_temp*t)
+
+def h(t):
+
+    if(t < 10*math.pi):
+
+        return math.exp(-2*t*t/(math.pi*math.pi))*math.cos(math.pi*t)
+
+    else: 
+        return 0
+
+def JordanWigner_ci(i,num):
+
+    dim = 2**num
+
+    X = mycode.SingleX(i,num)
+    Z = mycode.SingleZ(i,num)
+    Y = 1j*X.dot(Z)
+
+    Sum_Z = np.zeros((dim,dim),dtype = complex)
+    for j in range(i):
+        Sum_Z = Sum_Z + mycode.SingleZ(j,num) - np.identity(dim)
+    Product_Z = scipy.linalg.expm(1j*math.pi*Sum_Z/2)
+
+
+    return Product_Z.dot(X + 1j*Y)/2
+
+def JordanWinger_neighbor_hopping(i,num):
+
+
+    XX = mycode.XX_pair(num,i,i+2)
+    YY = mycode.YY_pair(num,i,i+2)
+    Z = mycode.SingleZ(i+1,num)
+
+    return 0.5*(XX + YY).dot(Z)
+
+def JordanWigner_ni(i,num):
+
+    dim = 2**num 
+
+    return 0.5*(np.identity(dim) - mycode.SingleZ(i,num))
+
+
+def find_closest_idx(energy_data,k):
+
+    new_list = np.zeros(len(energy_data))
+    for i in range(len(energy_data)):
+        new_list[i] = abs(energy_data[i] - k)
+
+    return np.argmin(new_list)
+
+
+def signal(frequencies, amplitudes,t):
+
+    s = 0 + 1j*0 
+    L = len(frequencies)
+
+    for l in range(L):
+        s = s + amplitudes[l] * cmath.exp(1j*frequencies[l] * t)
+
+    return s
+
+def trace_distance(r1,r2):
+
+    d = len(r1[0])
+
+    dr = r1 - r2 
+
+    eig,vec = np.linalg.eig(dr)
+    output = 0
+    for i in range(d):
+        output = output + abs(eig[i])
+
+    return output /2
+
+
+ham = np.load('ham.npy')
+A = np.load('A0.npy')
+P = np.load('Parity.npy')
+ground_state = np.load('ground_state.npy')
+
+
+dim = len(ham[0])
+
+T0 = np.load('T0.npy')
+T1 = np.load('T1.npy')
+T2 = np.load('T2.npy')
+T3 = np.load('T3.npy')
+
+num_T = 1000
+nT = 50
+T= nT*math.pi
+dt = T/num_T
+
+
+T_axis = np.zeros(num_T)
+
+Y1_axis = np.zeros(num_T)
+Y2_axis = np.zeros(num_T) 
+
+O1_axis = np.zeros(num_T)
+O2_axis = np.zeros(num_T)
+
+
+noisy_init_state = np.load('new_init_state.npy')
+print(trace_distance(noisy_init_state,ground_state))
+
+
+Id= np.identity(dim)
+Kick_Operator1 = (Id + 1j*A)/math.sqrt(2)
+kicked_state1 = Kick_Operator1.dot((ground_state).dot(np.conj(Kick_Operator1).T))
+kicked_state2 = Kick_Operator1.dot((noisy_init_state).dot(np.conj(Kick_Operator1).T))
+
+
+Kick_Operator2 = (P + A)/math.sqrt(2)
+kicked_state3 = Kick_Operator2.dot((ground_state).dot(np.conj(Kick_Operator2).T))
+kicked_state4 = Kick_Operator2.dot((noisy_init_state).dot(np.conj(Kick_Operator2).T))
+
+
+
+U_Tro_dt = scipy.linalg.expm(-1j*T3*dt)
+U_Tro_dt = scipy.linalg.expm(-1j*T2*dt).dot(U_Tro_dt)
+U_Tro_dt = scipy.linalg.expm(-1j*T1*dt).dot(U_Tro_dt)
+U_Tro_dt = scipy.linalg.expm(-1j*T0*dt).dot(U_Tro_dt)
+
+U_Ext_dt = scipy.linalg.expm(-1j*ham*dt)
+
+At_Tro = A.copy()
+At_Ext = A.copy()
+
+for n in range(num_T):
+
+    print(n)
+
+    t = (n+1)*dt
+    At_Tro = (np.conj(U_Tro_dt).T).dot(At_Tro.dot(U_Tro_dt))
+    At_Ext = (np.conj(U_Ext_dt).T).dot(At_Ext.dot(U_Ext_dt))
+
+
+
+    w1 = mycode.trace(kicked_state1.dot(At_Ext)).real
+    w2 = mycode.trace(kicked_state2.dot(At_Tro)).real
+
+    v1 = mycode.trace(kicked_state3.dot(At_Ext)).real
+    v2 = mycode.trace(kicked_state4.dot(At_Tro)).real
+
+    T_axis[n] = t
+
+    O1_axis[n] = w1
+    O2_axis[n] = w2
+
+    Y1_axis[n] = v1
+    Y2_axis[n] = v2
+
+
+plt.plot(T_axis, O1_axis,label = 'Im, Exact + Exact')
+plt.plot(T_axis, O2_axis,label = 'Im, Noisy + Trotter')
+
+plt.plot(T_axis, Y1_axis,label = 'Re, Exact + Exact')
+plt.plot(T_axis, Y2_axis,label = 'Re, Noisy + Trotter')
+
+np.save('T_axis.npy',T_axis)
+np.save('signal_Im_ExtExt.npy',O1_axis)
+np.save('signal_Im_NoiTro.npy',O2_axis)
+
+np.save('signal_Re_ExtExt.npy',Y1_axis)
+np.save('signal_Re_NoiTro.npy',Y2_axis)
+
+plt.legend()
+plt.show()
+
+
+
